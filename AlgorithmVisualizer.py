ec929468a8e279029a8f0ce1fc5a3b62f1e4de40
Janar Kaljula <janarkaljula1@gmail.com>
2024-08-06 09:53:36 +0300

    Initial DrawInformation class finished

--- AlgorithmVisualizer.py
+++ AlgorithmVisualizer.py
@@ -10,10 +10,22 @@ class DrawInformation:
     GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
     
+    SIDE_PAD = 100
+    TOP_PAD = 150
+    
     def __init__(self, width, height, lst):
         self.width = width
         self.height = height
         
         self.window = pygame.display.set_mode(width, height)
         pygame.display.set_caption("Sorting algorithm visualizer")
+        self.set_list(lst)
         
+    def set_list(self, lst):
+        self.lst = lst
+        self.max_val = max(lst)
+        self.min_val = min(lst)
+        
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
